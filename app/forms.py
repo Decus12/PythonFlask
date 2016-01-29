1162a493993ad172a8e0b28b364eb0f02049c225
mrmmathias <dix_mccoy@yahoo.com>
2016-01-29 14:49:13 +0200

    register and sqldatabase

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import StringField,PasswordField, SubmitField
+from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email
 
 class LoginForm(Form):
-    email = StringField('Enter your Email',validators=[Required(),Email()])
-    passw = PasswordField('Enter password',validators=[Required()])
-    submit = SubmitField('Login')
+	email = StringField('Enter your email',validators=[Required(),Email()])
+	passw = PasswordField('Enter password',validators=[Required()])
+	submit = SubmitField('Login')
+    
+class RegisterForm(Form):
+	email = StringField('Enter your email',validators=[Required(),Email()])
+	passw = PasswordField('Enter password',validators=[Required()])
+	submit = SubmitField('Register')
